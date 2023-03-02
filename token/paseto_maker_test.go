1a33a0571334ea1559b7f8b71cf6cce474f2d21f
Luis Ogando <logando1193@gmail.com>
2023-03-02 17:54:30 -0400

    Added implementation for JWT and PASETO

---
+++ token/paseto_maker_test.go
@@ -0,0 +1,56 @@
+package token
+
+import (
+	"testing"
+	"time"
+
+	"github.com/logando/simplebank/util"
+	"github.com/stretchr/testify/require"
+)
+
+func TestPasetoMaker(t *testing.T) {
+	maker, err := NewPasetoMaker(util.RandomString(32))
+	require.NoError(t, err)
+
+	username := util.RandomOwner()
+	duration := time.Minute
+
+	issueAt := time.Now()
+	expireAt := issueAt.Add(duration)
+
+	token, payload, err := maker.CreateToker(username, duration)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+	require.NotEmpty(t, payload)
+
+	payload, err = maker.VerifyToken(token)
+	require.NoError(t, err)
+	require.NotEmpty(t, payload)
+
+	require.NotZero(t, payload.ID)
+	require.Equal(t, username, payload.Username)
+	require.WithinDuration(t, issueAt, payload.IssuedAt, time.Second)
+	require.WithinDuration(t, expireAt, payload.ExpiredAt, time.Second)
+
+}
+
+func TestExpiredPasetoToken(t *testing.T) {
+	maker, err := NewPasetoMaker(util.RandomString(32))
+	require.NoError(t, err)
+
+	token, payload, err := maker.CreateToker(util.RandomOwner(), -time.Minute)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+	require.NotEmpty(t, payload)
+
+	payload, err = maker.VerifyToken(token)
+	require.Error(t, err)
+	require.EqualError(t, err, ErrExpiredToken.Error())
+	require.Nil(t, payload)
+}
+
+func TestInvalidPasetoLengthToken(t *testing.T) {
+	maker, err := NewPasetoMaker(util.RandomString(7))
+	require.Nil(t, maker)
+	require.Error(t, err)
+}
